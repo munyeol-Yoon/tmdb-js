b1e4063fddaf3b58cbc3004b03b8b1f16ccd1a8e
muyeon <gmakin36@gmail.com>
2024-04-23 18:04:46 +0900

    add pagenation feature

--- js/indexScript.js
+++ js/indexScript.js
@@ -95,7 +95,7 @@ function displayModal(movieObj) {
     <h2>${movieObj.title}</h2>
     <img src="${movieObj.img}" />
     <p>${movieObj.overview}</p>
-    <p>${movieObj.rating}</p>
+    <p>Rating: ${movieObj.rating}</p>
     <button class="close">Close</button>
   `;
 
@@ -120,4 +120,49 @@ function toggleContainer() {
   const container = document.querySelector(".container");
   container.style.display =
     container.style.display === "none" ? "flex" : "none";
+}
+
+const $prevPageBtn = document.querySelector("#prevPage");
+const $nextPageBtn = document.querySelector("#nextPage");
+const $currentPage = document.querySelector("#currentPage");
+
+let currentPage = 1;
+let isFetchingData = false;
+
+$prevPageBtn.addEventListener("click", () => {
+  if (!isFetchingData && currentPage > 1) {
+    currentPage--;
+    fetchData(currentPage);
+    scrollToTop();
+  }
+});
+
+$nextPageBtn.addEventListener("click", () => {
+  if (!isFetchingData) {
+    currentPage++;
+    fetchData(currentPage);
+    scrollToTop();
+  }
+});
+
+function fetchData(page) {
+  isFetchingData = true;
+  fetch(
+    `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=${page}&sort_by=popularity.desc`,
+    options
+  )
+    .then((response) => response.json())
+    .then((response) => {
+      displayFindAllAndSearchResults(response);
+      $currentPage.textContent = page;
+      isFetchingData = false;
+    })
+    .catch((err) => {
+      console.error(err);
+      isFetchingData = false;
+    });
+}
+
+function scrollToTop() {
+  window.scrollTo({ top: 0, behavior: "smooth" });
 }
